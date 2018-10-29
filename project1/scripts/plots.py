a2c9db6f5052dcaeaf8f29f8c43195e599fd27d1
Joel Fischer <joel.fischer@epfl.ch>
2018-10-29 18:12:42 +0100

    exit for stoch log reg

---
+++ project1/scripts/plots.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""a function of ploting figures."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def cross_validation_visualization(lambds, mse_tr, mse_te,label="", color1 = "r", color2 = "b"):
+    """visualization the curves of mse_tr and mse_te."""
+    plt.semilogx(lambds, mse_tr, marker=".", color=color1, label='train error' + " " + label)
+    plt.semilogx(lambds, mse_te, marker=".", color=color2, label='test error' + " " + label)
+    plt.xlabel("lambda")
+    plt.ylabel("rmse")
+    plt.title("cross validation")
+    plt.legend(loc=2)
+    plt.grid(True)
+    plt.savefig("cross_validation")
+
+
+def bias_variance_decomposition_visualization(degrees, rmse_tr, rmse_te):
+    """visualize the bias variance decomposition."""
+    rmse_tr_mean = np.expand_dims(np.mean(rmse_tr, axis=0), axis=0)
+    rmse_te_mean = np.expand_dims(np.mean(rmse_te, axis=0), axis=0)
+    plt.plot(
+        degrees,
+        rmse_tr.T,
+        'b',
+        linestyle="-",
+        color=([0.7, 0.7, 1]),
+        label='train',
+        linewidth=0.3)
+    plt.plot(
+        degrees,
+        rmse_te.T,
+        'r',
+        linestyle="-",
+        color=[1, 0.7, 0.7],
+        label='test',
+        linewidth=0.3)
+    plt.plot(
+        degrees,
+        rmse_tr_mean.T,
+        'b',
+        linestyle="-",
+        label='train',
+        linewidth=3)
+    plt.plot(
+        degrees,
+        rmse_te_mean.T,
+        'r',
+        linestyle="-",
+        label='test',
+        linewidth=3)
+    plt.ylim(0.2, 0.7)
+    plt.xlabel("degree")
+    plt.ylabel("error")
+    plt.title("Bias-Variance Decomposition")
+    plt.savefig("bias_variance")
