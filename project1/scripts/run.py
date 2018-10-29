e955e4fffad9cf952d10d7750c941d4397854771
Patrick Ley <okhofsk@gmail.com>
2018-10-29 22:28:00 +0100

    this is the end

--- project1/scripts/run.py
+++ project1/scripts/run.py
@@ -6,7 +6,7 @@ path_tr = "../data/train.csv"
 yb_tr, data_tr, ids_tr = load_csv_data(path_tr,False)
 
 degree = 7
-data_tr0 = poly_expansion(data_tr, degree, False, False)
+data_tr0 = poly_expansion(data_tr, degree, False, True)
 data_tr0, mean_tr, std_tr = standardize(data_tr0)
 data_tr0 = add_constant(data_tr0)
 nfeatures = data_tr0.shape[1]
@@ -16,11 +16,12 @@ w_SGD, loss_SGD = my_stoch_logistic_regression(yb_tr, data_tr0, initial_w, 2000,
 
 path_te = "../data/test.csv"
 yb_te, data_te, ids_te = load_csv_data(path_te,False)
-data_te0 = poly_expansion(data_te, degree, False, False)
+data_te0 = poly_expansion(data_te, degree, False, True)
 data_te0, mean_te, std_te = standardize(data_te0, mean_tr, std_tr)
 data_te0 = add_constant(data_te0)
 yb_te = yb_te.reshape(len(yb_te),1)
 
 y_pred = compute_y(data_te0, w_SGD)
+print(compute_loss(yb_tr, data_tr0, w_SGD, "log", 0))
 
 create_csv_submission(ids_te, y_pred, "Submission.csv")
