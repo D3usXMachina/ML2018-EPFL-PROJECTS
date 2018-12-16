494ce34a84b0f29b68ae24bc38db4df110afb380
Joel Fischer <joel.fischer@epfl.ch>
2018-12-16 18:35:11 +0100

    nc

--- project2/project_recommender_system/code/utility.py
+++ project2/project_recommender_system/code/utility.py
@@ -5,22 +5,25 @@
 # authors:                             Patrick Ley, Joel Fischer
 # date:                                14.12.2018
 # ==============================================================================
-# This code collection contains some functions used for our realsation of
+# This code collection contains some functions used for our realisation of
 # project2: recommender system.
 # ==============================================================================
-# - File processing and Import/Export
+# - File processing and Import
 #   -> loadData2df(infilepath,skiplines=1)
 #   -> loadData2ds(source,skiplines=1)
+# - Predictions and Parameter export
 #   -> generatePredictions(algo,pred_df,verbose)
-#   -> exportPredictions(outfilepath,pred_df)
+#   -> addDateAndTime(filepath,add_date=True,add_time=True)
+#   -> exportPredictions(pred_df,outfilepath=None,add_date=False,add_time=False)
+#   -> exportAlgoParameters(algo,outfilepath=None,add_date=False,add_time=False)
 #
 # - Deprecated methods
 #   ((DEPRECATED) -> convertCSV2Surprise(infilepath,outfilepath=None) )
 #   ((DEPRECATED) -> properCSV(infilepath,outfilepath=None,seperator=None) )
 # ==============================================================================
 # TODO:
-#   - nothing yet
-#   -
+#   - add function to export gridseach/cross-validation data
+#   - add function to plot gridsearch/cross-validation data
 # ==============================================================================
 
 import pandas as pd
@@ -31,7 +34,7 @@ from surprise import Dataset
 from surprise import Reader
 
 # ==============================================================================
-# File processing and Import/Export
+# File processing and Import
 # ==============================================================================
 # Some functions for reformatting and importing/exporting user-item-rating data.
 # ------------------------------------------------------------------------------
@@ -92,107 +95,6 @@ def loadData2df(infilepath,skiplines=1):
 
 # ------------------------------------------------------------------------------
 
-def generatePredictions(algo,pred_df,verbosity=0):
-    """
-    ============================================================================
-    Predict ratings .
-    ----------------------------------------------------------------------------
-    ----------------------------------------------------------------------------
-    Input:
-
-    - outfilepath   (string,valid path);
-                    filepath to the newly created file;
-
-    - pred_df       (pandas.DataFrame);
-                    DataFrame containing the predicted ratings
-
-    - verbosity     (integer);
-                    print out more information the higher the integer:
-                    0 : no text output
-                    1 : print (#rating,uid,iid,rating)
-                    2 : also print verbose predict output
-
-    Output: (nothing)
-    ============================================================================
-    """
-
-    verb = (verbosity>1)
-    for i in range(pred_df.shape[0]):
-        pred_df.at[i,"rating"] = algo.predict(str(pred_df.at[i,"userId"])\
-                                             ,str(pred_df.at[i,"itemId"]),verbose=verb).est
-        if(verbosity>0):
-            print(i,pred_df["userId"][i],pred_df["itemId"][i],pred_df["rating"][i])
-
-    return
-
-# ------------------------------------------------------------------------------
-
-def addDateAndTime(outfilepath,add_date=False,add_time=False):
-
-    if add_date or add_time:
-        datestring = str(datetime.datetime.now())
-        datestring = datestring.replace(" ","_")
-        datestring = datestring.replace(":","")
-        if not add_time:
-            datestring = datestring[:-5]
-        if not add_date:
-            datestring = datestring[11:]
-        outfilepath = outfilepath.replace(".","_"+a+".")
-
-    return outfilepath
-
-def exportPredictions(outfilepath,pred_df,add_date=False,add_time=False):
-    """
-    ============================================================================
-    Export predictions from a pandas.DataFrame to a file in the format demanded
-    by the automatic evaluation algorithm.
-    ----------------------------------------------------------------------------
-    ----------------------------------------------------------------------------
-    Input:
-
-    - outfilepath   (string,valid path);
-                    filepath to the newly created file;
-
-    - pred_df       (pandas.DataFrame);
-                    DataFrame containing the predicted ratings
-
-    Output: (nothing)
-    ============================================================================
-    """
-    outfilepath = addDateAndTime(outfilepath,add_date,add_time)
-
-    outfile = open(outfilepath,"wt")
-    outfile.write("Id,Prediction\n")
-    for i, rating in pred_df.iterrows():
-        outfile.write("r"+str(int(rating["userId"]))+"_c"+str(int(rating["itemId"]))+","+str(round(rating["rating"]))+"\n")
-
-    outfile.close()
-
-    return
-
-def exportAlgoParameters(algo,outfilepath=None,add_date=False,add_time=False):
-
-    if outfilepath is None:
-        outfilepath = "algoPara.csv"
-        outfilepath = addDateAndTime(outfilepath,True,True)
-    else:
-        outfilepath = addDateAndTime(outfilepath,add_date,add_time)
-
-    outfile = open(outfilepath,"w")
-
-    attlist = dir(algo)
-    for att in attlist:
-        if att[0] != "_" and not callable(getattr(algo,att)):
-            outfile.write(att+":"+str(type(getattr(algo,att)))+"\n")
-            outfile.write(str(getattr(algo,att)))
-            outfile.write("\n")
-
-    outfile.close()
-
-    return
-
-# ------------------------------------------------------------------------------
-
 def loadData2ds(source=None,skiplines=1):
     """
     ============================================================================
@@ -231,6 +133,194 @@ def loadData2ds(source=None,skiplines=1):
     ds = Dataset.load_from_df(source,reader)
 
     return ds
+
+# ==============================================================================
+# Predictions and Parameter export
+# ==============================================================================
+# Some functions to automate predictions and algorithm parameter export.
+# ------------------------------------------------------------------------------
+
+
+def generatePredictions(algo,pred_df,verbosity=0):
+    """
+    ============================================================================
+    Predict ratings .
+    ----------------------------------------------------------------------------
+    ----------------------------------------------------------------------------
+    Input:
+
+    - outfilepath   (string,valid path);
+                    filepath to the newly created file;
+
+    - pred_df       (pandas.DataFrame);
+                    DataFrame containing the predicted ratings
+
+    - verbosity     (integer);
+                    print out more information the higher the integer:
+                    0 : no text output
+                    1 : print (#rating,uid,iid,rating)
+                    2 : also print verbose predict output
+
+    Output: (nothing)
+    ============================================================================
+    """
+    raw_id_type = type(algo.trainset.to_raw_iid(0))
+    verb = (verbosity>1)
+    iid = 0
+    uid = 0
+    for i in range(pred_df.shape[0]):
+        uid = pred_df.at[i,"userId"]
+        iid = pred_df.at[i,"itemId"]
+        if raw_id_type is str:
+            uid = str(uid)
+            iid = str(iid)
+        pred_df.at[i,"rating"] = algo.predict(uid,iid,verbose=verb).est
+        if(verbosity>0):
+            print(i,pred_df["userId"][i],pred_df["itemId"][i],pred_df["rating"][i])
+
+    return
+
+# ------------------------------------------------------------------------------
+
+def addDateAndTime(filepath,add_date=True,add_time=True):
+    """
+    ============================================================================
+    Add date and time to a filename.
+    ----------------------------------------------------------------------------
+    Adds the current date and time in the filepath before the filename extension
+    in the format "_YYYY-MM-DD_HHMM".
+    example:
+    "../path/filename.extension" -> "../path/filename_<date>_<time>.extension"
+    ----------------------------------------------------------------------------
+    Input:
+
+    - filepath      (string,valid path);
+                    filepath to be modified;
+
+    - add_date      (boolean);
+                    add date to filename;
+                    (default=False);
+
+    - add_time      (boolean);
+                    add time to filename;
+                    (default=False);
+
+    Output:
+
+    - filepath_new  (string);
+                    the modified filepath
+    ============================================================================
+    """
+
+    filepath_new = filepath
+    if add_date or add_time:
+        datestring = str(datetime.datetime.now())[:16]
+        datestring = datestring.replace(" ","_")
+        datestring = datestring.replace(":","")
+        if not add_time:
+            datestring = datestring[:-5]
+        if not add_date:
+            datestring = datestring[11:]
+        name, extension = filepath_new.rsplit(".",1)
+        filepath_new = name + "_" + datestring + "." + extension
+
+    return filepath_new
+
+# ------------------------------------------------------------------------------
+
+def exportPredictions(pred_df,outfilepath=None,add_date=False,add_time=False):
+    """
+    ============================================================================
+    Export predictions from a pandas.DataFrame to a file in the format demanded
+    by the automatic evaluation algorithm.
+    ----------------------------------------------------------------------------
+    ----------------------------------------------------------------------------
+    Input:
+
+    - outfilepath   (string,valid path);
+                    filepath to the newly created file;
+                    (default="../data/submissions/submission_<datetime>.csv");
+
+    - pred_df       (pandas.DataFrame);
+                    DataFrame containing the predicted ratings;
+
+    - add_date      (boolean);
+                    add date to filename;
+                    (default=False);
+
+    - add_time      (boolean);
+                    add time to filename;
+                    (default=False);
+
+    Output: (nothing)
+    ============================================================================
+    """
+
+    if outfilepath is None:
+        outfilepath_new = "../data/submissions/submission.csv"
+        outfilepath_new = addDateAndTime(outfilepath_new,True,True)
+    else:
+        outfilepath_new = outfilepath
+        outfilepath_new = addDateAndTime(outfilepath_new,add_date,add_time)
+
+    outfile = open(outfilepath_new,"wt")
+    outfile.write("Id,Prediction\n")
+    for i, rating in pred_df.iterrows():
+        outfile.write("r"+str(int(rating["userId"]))+"_c"+str(int(rating["itemId"]))+","+str(round(rating["rating"]))+"\n")
+
+    outfile.close()
+
+    return
+
+# ------------------------------------------------------------------------------
+
+def exportAlgoParameters(algo,outfilepath=None,add_date=False,add_time=False):
+    """
+    ============================================================================
+    Export the parameters used by the algo.
+    ----------------------------------------------------------------------------
+    ----------------------------------------------------------------------------
+    Input:
+
+    - pred_df       (surprise.BaseAlgo);
+                    surprise algorithm used to make predictions;
+
+    - outfilepath   (string,valid path);
+                    filepath to the newly created file;
+                    (default="../data/parameters/parameters_<datetime>.dat");
+
+    - add_date      (boolean);
+                    add date to filename;
+                    (default=False);
+
+    - add_time      (boolean);
+                    add time to filename;
+                    (default=False);
+
+    Output: (nothing)
+    ============================================================================
+    """
+
+    if outfilepath is None:
+        outfilepath_new = "../data/parameters/parameters.dat"
+        outfilepath_new = addDateAndTime(outfilepath_new,True,True)
+    else:
+        outfilepath_new = outfilepath
+        outfilepath_new = addDateAndTime(outfilepath_new,add_date,add_time)
+
+    outfile = open(outfilepath_new,"w")
+
+    outfile.write("algo:"+str(getattr(algo,"__class__")))
+    attlist = dir(algo)
+    for att in attlist:
+        if att[0] != "_" and not callable(getattr(algo,att)):
+            outfile.write(att+":"+str(type(getattr(algo,att)))+"\n")
+            outfile.write(str(getattr(algo,att)))
+            outfile.write("\n")
+
+    outfile.close()
+
+    return
 
 # ==============================================================================
 # Deprecated Methods
